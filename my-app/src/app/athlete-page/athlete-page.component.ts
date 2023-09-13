6a1a1ad6f99b67274ccf0a3af55e9ff09decdaca
staplKody <kody.byrd@staplcotn.com>
2023-09-13 16:20:10 -0500

    FEAT: filtering weight classes by name and OPEN

--- my-app/src/app/athlete-page/athlete-page.component.ts
+++ my-app/src/app/athlete-page/athlete-page.component.ts
@@ -2,23 +2,67 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data-service.service';
 import { WeightClass,PullerModel } from '../interfaces/interfaces';
 
-
-
 @Component({
   selector: 'app-athlete-page',
   templateUrl: './athlete-page.component.html',
   styleUrls: ['./athlete-page.component.css']
 })
 export class AthletePageComponent implements OnInit {
- weightClasses: WeightClass[] = [];
- searchNames: string[] = ['KODY BYRD', 'DUSTIN HYATT', 'HUNTER WELCH'];
- filteredWeightClasses: WeightClass[] = [];
+  weightClasses: WeightClass[] = [];
+  searchNames: string[] = ['TREVOR CLAUNCH'
+    ,'AUSIN ELZY'
+    , 'DUSTIN HYATT'
+    , 'JACKSON LOTHENORE'
+    , 'WILL SMITH'
+    , 'WARDELL FERGUSON'
+    , 'CHETT BURGE'
+    , 'DILLON THOMPSON'
+    , 'ADAM GREGORY'
+    , 'BRIAN CALHOUN'
+    , 'KODY BYRD'
+    , 'LATHE WARD'
+    , 'SEAN HANCOCK'
+    , 'JEREMY THORTON'
+    //, 'JOSH DAVIS' NO ENTRIES
+    , 'EVAN BEDWELL'
+    , 'BURT WILKERSON'
+    , 'RANCE CLAYTON'
+    , 'BRANDON SMITH'
+    , 'MASON WILLIAMS'
+    , 'SEAN KEEN'
+    , 'JUSTIN WHITE'
+    , 'DAKOTA THOMPSON'
+    , 'CHASE PORCH'
+    , 'BRAD MANN'
+    , 'WAYNE WITHERS'
+    , 'BJ FOKAKIS'
+    , 'BRANDON MANN'
+    , 'TRENT DAVIS'
+    , 'STUART HYATT'
+    , 'JOSH JARVIS'
+    , 'HUNTER MCNEELY'
+    , 'JOASH DENNIS'
+    , 'STEVEN CHAMBERS'
+  ];
+  filteredWeightClasses: WeightClass[] = [];
 
- constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService) {}
 
- ngOnInit(): void {
+  ngOnInit(): void {
     this.dataService.getJsonData().subscribe((jsonData) => {
       this.weightClasses = this.dataService.processData(jsonData);
-    })
- }
+      this.filterWeightClasses();
+    });
+  }
+
+  filterWeightClasses() {
+    this.filteredWeightClasses = this.weightClasses.map((weightClass) => ({
+      ...weightClass,
+      pullers: weightClass.pullers.filter((puller) => {
+        const fullName = `${puller.firstName} ${puller.lastName}`;
+        return this.searchNames.includes(fullName.toUpperCase());
+      })
+    }));
+  }
 }
+
--- my-app/src/app/data-service.service.ts
+++ my-app/src/app/data-service.service.ts
@@ -18,26 +18,22 @@ export class DataService {
 
   processData(jsonData: any): WeightClass[] {
     const bracketPullerArray = jsonData.bracketPullers; // Use the correct property name
-    const searchNames: string[] = ['KODY BYRD', 'DUSTIN HYATT', 'BJ FOKAKIS'];
+    
     const weightClasses: WeightClass[] = [];
   
     for (const item of bracketPullerArray) {
-      const weightClass: WeightClass = {
-        arm: item.arm,
-        weight: item.weight,
-        pullers: item.pullerModels
-      };
-      weightClasses.push(weightClass);
+      if(item.division == 'OPEN'){
+        const weightClass: WeightClass = {
+          arm: item.arm,
+          weight: item.weight,
+          pullers: item.pullerModels
+        };
+        weightClasses.push(weightClass);
+      }
     }
     console.log(weightClasses);
     return weightClasses; 
   }
   
-  searchWeightClasses(): void{
-    const filteredWeightClasses: WeightClass[] = [];
-    //find desired pullers in the weightClasses array based off of names in searchNames array
-    
-    
-  }
   
 }
